1f3cbf58e2c7191f7948b44c5eaf9a17b64fd89e
Fire30 <tjcorley30@hotmail.com>
2014-07-28 13:42:29 -0400

    Added set memory function

--- pyxdevkit/console.py
+++ pyxdevkit/console.py
@@ -28,9 +28,20 @@ class Console(object):
 		# The first thing returned will be 203- binary response follows\r\n, + the data.
 		# If the length is small it will be returned all in one recv.
 		# If it is larger it will take multiple.
+		# Note: The first two bytes of the binary response are not part of the memory
 		received = sock.recv(4096 + length)
-		received = received.split('\r\n')[1]
+		received = received.split('\r\n')[1][2:]
 		while len(received) < length:
-			# The first two bytes of the binary response are not part of the mem
 			received += sock.recv(length)[2:]
 		return received
+	def set_mem(self,addr,data):
+		""" Sets the memory at addr to data
+			The value in data has to be a string of hexadecimal characters
+			so for example data = 'DEADBEEF' is fine
+		"""
+		# Set up the socket and connect
+		HOST, PORT = self.ip_address, 730
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((HOST, PORT))
+		# This is the cmd that we send to the console
+		sock.send("SETMEM ADDR=0x%x DATA=%s\r\n" % (addr,data))

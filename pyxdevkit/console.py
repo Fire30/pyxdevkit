1d91c1f1c9eb07db05a4ec2f6b827f5416f16333
Fire30 <tjcorley30@hotmail.com>
2014-07-27 22:36:15 -0400

    Created console object

--- pyxdevkit/console.py
+++ pyxdevkit/console.py
@@ -6,3 +6,8 @@ pyxdevkit.console
 
 module that implements xdevkit's methods.
 """
+class Console(object):
+	"""object that contains the functions that implement xdevkit"""
+	def __init__(self, ip_address):
+		# Since sdk is not neccasrily installed we use ip address not name to connect
+		self.ip_address = ip_address

3071c4686b717e01e5c348101ac8e114c1c28dea
Fire30 <tjcorley30@hotmail.com>
2014-07-27 21:50:14 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyxdevkit',
+    version='0.1dev',
+    packages=['pyxdevkit',],
+    license='MIT',
+    author='T.J. Corley',
+    author_email='tjcorley30@hotmail.com',
+    description='xdevkit for python.',
+    long_description=open('README.MD').read(),
+)

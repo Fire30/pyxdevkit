d8b8323c7795fe104541ebb8dc873387498cf563
Fire30 <tjcorley30@hotmail.com>
2014-07-29 18:15:02 -0400

    Add basic notifications

--- pyxdevkit/debugger.py
+++ pyxdevkit/debugger.py
@@ -8,12 +8,19 @@ module that implements the debugger features of xdevkit.
 """
 import socket
 import sys
+from threading import Thread
+import select
+import threading
 
 class Debugger(object):
 
 	def __init__(self,ip_address,socket):
-		self.ip_address = addr
+		self.ip_address = ip_address
 		self.socket = socket
+		self.on_std_notify_func = None
+		self.thread = Thread(target=self.listen_for_notify)
+		self.thread.setName('debug_thread')
+		self.thread.start()
 
 	def set_breakpoint(self, addr):
 		""" Sets a breakpoint at addr. 
@@ -38,3 +45,21 @@ class Debugger(object):
 		# This is the cmd that we send to the console
 		sock.send("GO\r\n" % addr)
 		sock.close()
+
+	def listen_for_notify(self):
+		""" When the debugger is connected, the console periodically sends back 
+			notifications that the used can handle. We just pass them to the 
+			on_std_notify_func function.
+		"""
+		while True:
+			for i in threading.enumerate():
+   				if i.name == "MainThread" and not i.isAlive():
+   					self.socket.close()
+   					return
+			self.socket.setblocking(0)
+			ready = select.select([self.socket], [], [], .3)
+			if ready[0]:
+				data = self.socket.recv(2048)
+				if self.on_std_notify_func:
+					self.on_std_notify_func(data)
+

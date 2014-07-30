6f0c0deb63fa6bc1bb313cba3a65f1e1dbe75429
Fire30 <tjcorley30@hotmail.com>
2014-07-30 00:12:17 -0400

    Fixed notifications and added classes EventInfo and XboxThread classes

--- pyxdevkit/debugger.py
+++ pyxdevkit/debugger.py
@@ -11,6 +11,7 @@ import sys
 from threading import Thread
 import select
 import threading
+from event import EventInfo
 
 class Debugger(object):
 
@@ -43,26 +44,32 @@ class Debugger(object):
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.connect((HOST, PORT))
 		# This is the cmd that we send to the console
-		sock.send("GO\r\n" % addr)
+		sock.send("GO\r\n")
 		sock.close()
 
 	def listen_for_notify(self):
 		""" When the debugger is connected, the console periodically sends back 
 			notifications that the used can handle. We just pass them to the 
 			on_std_notify_func function.
-
-			TODO: Create EventInfo and EventType objects that act like thier
-			xdevkit equivalent.
 		"""
 		while True:
+			# We need to make sure that the main thread is still running
+			# If it is not then we need to stop
 			for i in threading.enumerate():
    				if i.name == "MainThread" and not i.isAlive():
    					self.socket.close()
    					return
+   			# We just wait until there is something to recieve
 			self.socket.setblocking(0)
 			ready = select.select([self.socket], [], [], .3)
 			if ready[0]:
-				data = self.socket.recv(2048)
+				# We pass the data of the notification to our function
+				# Not that our function takes a string and an EventInfo parameter
+				data = self.socket.recv(4096)
 				if self.on_std_notify_func:
-					self.on_std_notify_func(data)
+					for cmd in data.split('\r\n'):
+						if cmd:
+							e_type = cmd.split(' ')[0]
+							e_info = cmd.split(' ')[1:]
+							self.on_std_notify_func(e_type,EventInfo(e_info,self.ip_address))
 
---
+++ pyxdevkit/xbox_thread.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+pyxdevkit.xbox_thread
+~~~~~~~~~~~~~
+
+holds functions that an XboxThread can do
+"""
+import socket
+
+class XboxThread(object):
+	def __init__(self,addr,ip_addr):
+		self.addr = addr
+		self.ip_addr = ip_addr
+
+	def t_continue(self):
+		"""
+			Tells the xbox to make the thread continue
+			Thread has to be a 32 bit integer
+		"""
+		HOST, PORT = self.ip_addr, 730
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((HOST, PORT))
+		# This is the cmd that we send to the console
+		sock.send("CONTINUE THREAD=0x%x EXCEPTION\r\n" % self.addr)
+		sock.close()
+
+	def get_registers(self):
+		""" Returns a dict of all the registers in the xbox
+		    example keys of the dict are Gpr5, Vr12, etc.
+
+		    TODO: update comment with all registers
+		"""
+		HOST, PORT = self.ip_addr, 730
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((HOST, PORT))
+		# This is the cmd that we send to the console
+		sock.send("GETCONTEXT THREAD=0x%x CONTROL INT FP VR\r\n" % self.addr)
+		data = ''
+		# The character that is sent when everything is done is '.'
+		while '.' not in data:
+			data += sock.recv(8092)
+		sock.close()
+		# The general purpose registers all have 0q followed by a 64 bit int
+		# It is still in hex so I have to replace it with 0x so that python will understanf
+		data = data.replace('0q','0x')
+		registers = {}
+		data = data.split('\r\n')
+		for register in data:
+			#ex register will look like 'gpr5=0x000XX...'
+			register_split = register.split('=')
+			if len(register_split) > 1:
+				registers[register_split[0]] = eval(register_split[1])
+		return registers
